1e0ba3dddc8356e8d136faf99bc8bb95448a6aff
Maruf Fayziev <fayziyevmaruf4140@gmail.com>
2026-03-12 10:27:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Entry point script for ML-projects.
+
+This simple Python file ensures GitHub detects this
+repository as Python instead of only Jupyter Notebook.
+"""
+
+
+def main() -> None:
+    """Placeholder main function."""
+
+
+if __name__ == "__main__":
+    main()
